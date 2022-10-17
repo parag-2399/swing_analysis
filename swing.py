059280587d983a8dcf2269be18df9b78b4c51e12
parag-2399 <106873127+parag-2399@users.noreply.github.com>
2022-10-17 14:07:50 +0530

    Update swing.py

--- swing.py
+++ swing.py
@@ -52,8 +52,8 @@ if selected_interval in ('5m','15m','1h'):
     
     start_date2=st.date_input("START_DATE",value=d,disabled=True)
 else:
-    d= datetime.today() - timedelta(days=365,value=d)
-    start_date2=st.date_input("START_DATE")
+    d= datetime.today() - timedelta(days=365)
+    start_date2=st.date_input("START_DATE",value=d)
 end_date2=st.date_input("END_DATE")
 
 for ticker_symbol in sectors[sector]:
